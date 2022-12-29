79b986f8de70affb62b2cbdc8c06615c4b986327
gryph1231 <cliffordhill24@gmail.com>
2022-12-29 12:12:01 -0600

    Update prot.lua

--- Rubim-RH/Rotations/WR/prot.lua
+++ Rubim-RH/Rotations/WR/prot.lua
@@ -55,6 +55,7 @@ RubimRH.Spell[73] = {
       TitanicThrow                          = Spell(384090),
       TitansTorment                         = Spell(390135),
       WreckingThrow                         = Spell(384110),
+	  Massacre = Spell(281001),
       -- Buffs
       AvatarBuff                            = Spell(107574),
       BattleShoutBuff                       = Spell(6673),
@@ -75,9 +76,13 @@ RubimRH.Spell[73] = {
       AshenJuggernautBuff                   = Spell(335234),
       MercilessBonegrinder                  = Spell(335260),
       MercilessBonegrinderBuff              = Spell(346574),
+	  SeismicReverberation = Spell(382956),
       -- Pool
+	  VanguardsDetermination = Spell(394056),
+	  BarbaricTraining = Spell(390675),
       Pool                                  = Spell(999910),
     -- Abilities
+	SuddenDeath = Spell(29725),
   Devastate                             = Spell(20243),
   Execute                               = Spell(163201),
   ShieldBlock                           = Spell(2565),
@@ -202,19 +207,6 @@ local function UseItems()
     end
 end
 
-local function HealthPotionSelected()
-
-    local HealthPotionIDs = {
-        191380, 191379, 191378
-
-    }
-
-    for _, HealthPotionID in ipairs(HealthPotionIDs) do
-        if Item(HealthPotionID):IsUsable() then
-            return Item(HealthPotionID)
-        end
-    end
-end
 
 
 local function Defensive()
@@ -235,7 +227,7 @@ local function Defensive()
 	end
   
 	-- demoralizing_shout,if=!buff.last_stand.up&!buff.shield_wall.up&!buff.rallying_cry.up
-	if S.DemoralizingShout:IsCastable() and (not Player:Buff(S.LastStandBuff) and not Player:Buff(S.ShieldWallBuff) and not Player:Buff(S.RallyingCryBuff)) and (Player:HealthPercentage()<85 and Player:NeedPanicHealing() or Player:HealthPercentage()<70)then
+	if S.DemoralizingShout:IsCastable() and (not Player:Buff(S.LastStandBuff) and not Player:Buff(S.ShieldWallBuff) and not Player:Buff(S.RallyingCryBuff)) and (Player:HealthPercentage()<90 and Player:NeedPanicHealing() or Player:HealthPercentage()<70)then
 		return S.DemoralizingShout:Cast()
 	end
   
@@ -249,164 +241,7 @@ local function Defensive()
 	end
   end
   
-  local function Aoe()
-
-	-- ignore_pain,if=rage.deficit>=35&buff.ignore_pain.value<health.max*0.3
-	if S.IgnorePain:IsReady() and (Player:RageDeficit() >= 35 and IgnorePainValue() < Player:MaxHealth() * 0.3) then
-		return S.IgnorePain:Cast()
-	end
-	-- spear_of_bastion
-	if S.SpearofBastion:IsCastable() and Target:IsInRange(8) and RubimRH.CDsON() then
-	  SuggestRageDump(20)
-	  return S.SpearofBastion:Cast()
-	end
-	-- thunderous_roar
-	if S.ThunderousRoar:IsCastable() and Target:IsInRange(8) and RubimRH.CDsON() then
-	  SuggestRageDump(10)
-	  return S.ThunderousRoar:Cast()
-	end
-	-- ravager
-	if S.Ravager:IsCastable() and (Target:IsInRange(5) or Enemies8y>=2) and RubimRH.CDsON() then
-	  SuggestRageDump(10)
-	  return S.Ravager:Cast()
-	end
-	-- shockwave
-	-- if S.Shockwave:IsCastable() and Target:IsInRange(5) then
-	--   SuggestRageDump(10)
-	--   return S.Shockwave:Cast()
-	-- end
-	-- shield_charge
-	if S.ShieldCharge:IsCastable() and Target:IsInRange(5) then
-		return S.ShieldCharge:Cast()
-	end
-  
-	-- revenge,if=rage.deficit>30
-	if S.Revenge:IsReady() and (not ShouldPressShieldBlock()) and (Player:RageDeficit() > 30) then
-		return S.Revenge:Cast()
-	end
-	-- thunder_clap
-	if S.ThunderClap:IsCastable() and Target:IsInRange(8) then
-	  SuggestRageDump(5)
-	  return S.ThunderClap:Cast()
-	end
-	-- titanic_throw
-	if S.TitanicThrow:IsCastable() and RubimRH.CDsON() and Target:IsInRange(8) then
-		return S.TitanicThrow:Cast()
-	end
-	-- rend,if=!talent.thunderclap&!talent.blood_and_thunder
-	if S.Rend:IsReady() and (not ShouldPressShieldBlock()) and ((not S.ThunderClap:IsAvailable()) and not S.BloodandThunder:IsAvailable()) then
-		return S.Rend:Cast()
-	end
-	-- shield_slam
-	if S.ShieldSlam:IsCastable()  and Target:IsInRange(8) then
-	  SuggestRageDump(20)
-	  return S.ShieldSlam:Cast()
-	end
-	-- execute
-	if S.Execute:IsReady() and not ShouldPressShieldBlock() and Target:IsInRange(8) then
-		return S.Execute:Cast()
-	end
-	-- devastate
-	if S.Devastate:IsCastable() and Target:IsInRange(8) then
-		return S.Devastate:Cast()
-	end
-	-- impending_victory
-	if S.ImpendingVictory:IsReady() and Target:IsInRange(8) then
-		return S.ImpendingVictory:Cast()
-	end
-	-- storm_bolt
-	-- if S.StormBolt:IsCastable() then
-	--   if Cast(S.StormBolt, nil, nil, not Target:IsInRange(20)) then return "storm_bolt aoe 30"; end
-	-- end
-  end
-  
-  local function Generic()
-	-- ignore_pain,if=(rage.deficit>=35&buff.ignore_pain.value<health.max*0.3*0.5)|buff.ignore_pain.remains<gcd
-	if S.IgnorePain:IsReady() and ((Player:RageDeficit() >= 35 and IgnorePainValue() < Player:MaxHealth() * 0.3 * 0.5) or Player:BuffRemains(S.IgnorePain) < Player:GCD()) then
-		return S.IgnorePain:Cast()
-	end
-	-- ravager
-	if S.Ravager:IsCastable() and (Target:IsInRange(5) or Enemies8y>=2) and RubimRH.CDsON() then
-	  SuggestRageDump(10)
-	  return S.Ravager:Cast()
-	end
-	-- thunderous_roar
-	if S.ThunderousRoar:IsCastable() and Target:IsInRange(5) and RubimRH.CDsON() then
-	  SuggestRageDump(10)
-	  return S.ThunderousRoar:Cast()
-	end
-	-- spear_of_bastion
-	if S.SpearofBastion:IsCastable()  and Target:IsInRange(8) and RubimRH.CDsON() then
-	  SuggestRageDump(20)
-	  return S.SpearofBastion:Cast()
-	end
-	-- shield_charge
-	if S.ShieldCharge:IsCastable() and Target:IsInRange(5) then
-		return S.ShieldCharge:Cast()
-	end
-	-- shockwave
-	-- if S.Shockwave:IsCastable()  and Target:IsInRange(8) then
-	--   SuggestRageDump(10)
-	--   return S.Shockwave:Cast()
-	-- end
-	-- execute
-	if S.Execute:IsReady() and not ShouldPressShieldBlock()  and Target:IsInRange(8) then
-		return S.Execute:Cast()
-	end
-	-- shield_slam
-	if S.ShieldSlam:IsReady() and Target:IsInRange(8) then
-	  SuggestRageDump(20)
-	  return S.ShieldSlam:Cast()
-	end
-	-- thunder_clap,if=active_enemies>=2|(talent.rend&talent.blood_and_thunder)
-	if S.ThunderClap:IsCastable() and Target:IsInRange(8) and (Enemies8y >= 2 or (S.Rend:IsAvailable() and S.BloodandThunder:IsAvailable())) then
-	  SuggestRageDump(5)
-	  return S.ThunderClap:Cast()
-	end
-	if (not ShouldPressShieldBlock()) then
-	  -- rend,if=!talent.thunderclap&!talent.blood_and_thunder
-	  if S.Rend:IsReady() and ((not S.ThunderClap:IsAvailable()) and not S.BloodandThunder:IsAvailable()) then
-		return S.Rend:Cast()
-	end
-	  -- revenge,if=rage.deficit>30&active_enemies>=2
-	  if S.Revenge:IsReady() and (Player:RageDeficit() > 30 and Enemies8y >= 2) then
-		return S.Revenge:Cast()
-	end
-	end
-	-- titanic_throw,if=active_enemies>=2
-	if S.TitanicThrow:IsReady() and (Enemies8y >= 2) then
-		return S.TitanicThrow:Cast()
-	end
-	-- devastate
-	if S.Devastate:IsReady() then
-		return S.Devastate:Cast()
-	end
-	-- heroic_throw
-	if S.HeroicThrow:IsReady() and not Target:IsInRange(8) and Target:IsInRange(30) and (Target:AffectingCombat() or Player:AffectingCombat()) then
-		return S.HeroicThrow:Cast()
-	end
-	-- titanic_throw
-	if S.TitanicThrow:IsReady() then
-		return S.TitanicThrow:Cast()
-	end
-	-- thunder_clap
-	if S.ThunderClap:IsReady() and Target:IsInRange(8) then
-	  SuggestRageDump(5)
-	  return S.ThunderClap:Cast()
-	end
-	-- revenge,if=rage.deficit>30
-	if S.Revenge:IsReady() and Target:IsInRange(8) and (not ShouldPressShieldBlock()) and (Player:RageDeficit() > 30) then
-		return S.Revenge:Cast()
-	end
-	-- impending_victory
-	if S.ImpendingVictory:IsReady() then
-		return S.ImpendingVictory:Cast()
-	end
-	-- storm_bolt
-	-- if S.StormBolt:IsCastable() then
-	--   if Cast(S.StormBolt, nil, nil, not Target:IsInRange(20)) then return "storm_bolt generic 38"; end
-	-- end
-  end
+ 
   
   --- ======= ACTION LISTS =======
   local function APL()
@@ -452,9 +287,9 @@ end
 
 if not Player:AffectingCombat() then
 
-	if S.BattleShout:IsCastable() and not Player:BuffP(S.BattleShoutBuff) then
-		return S.BattleShout:Cast()
-	end
+	-- if S.BattleShout:IsCastable() and not Player:BuffP(S.BattleShoutBuff) then
+	-- 	return S.BattleShout:Cast()
+	-- end
 
 
 
@@ -478,8 +313,8 @@ end
 
 
  
-    if Player:HealthPercentage() <= 25 and Player:AffectingCombat() and IsUsableItem(191380) and
-        GetItemCooldown(191380) == 0 and GetItemCount(191380) >= 1
+if Player:HealthPercentage() <= 25 and Player:AffectingCombat() and (IsUsableItem(191379) or IsUsableItem(191378)  or IsUsableItem(191380)) and
+        (GetItemCooldown(191380) == 0 or GetItemCooldown(191379) == 0 or GetItemCooldown(191378) == 0) and (GetItemCount(191380) >= 1 or GetItemCount(191379) >= 1 or GetItemCount(191378) >= 1)
         and (not Player:InArena() and not Player:InBattlegrounds()) then
         return I.HPIcon:Cast()
     end
@@ -510,35 +345,15 @@ end
 		  return S.ShieldBlock:Cast()
 	  end
 	  
-
-	  if RubimRH.CDsON() and (Player:Buff(S.AvatarBuff) or not S.Avatar:IsAvailable()) then
-	
-		
-		if S.BloodFury:IsCastable() and RubimRH.CDsON() then
-		return S.BloodFury:Cast()
-	end
-				
-		if S.Berserking:IsCastable() and RubimRH.CDsON() then
-			return S.Berserking:Cast()
-	end
-	
-		if S.AncestralCall:IsCastable() and RubimRH.CDsON() then
-			return S.AncestralCall:Cast()
-		end
-	  end
-
-	  if S.Revenge:IsReady() and Target:IsInRange(8) and (Player:Buff(S.RevengeBuff) and (Target:HealthPercentage() > 20 or Enemies8y > 3) and S.ShieldSlam:CooldownRemains() > 0) then
-		return S.Revenge:Cast()
-	end
-
-	  if S.IgnorePain:IsReady() and IgnorePainWillNotCap() and (Target:HealthPercentage() >= 20 and (Player:Rage() >= 85 and S.ShieldSlam:CooldownUp() and Player:Buff(S.ShieldBlockBuff) or Player:Rage() >= 60 and S.DemoralizingShout:CooldownUp() and S.BoomingVoice:IsAvailable() or Player:Rage() >= 70 and S.Avatar:CooldownUp() or Player:Rage() >= 40 and S.DemoralizingShout:CooldownUp() and S.BoomingVoice:IsAvailable() and Player:Buff(S.LastStandBuff) or Player:Rage() >= 55 and S.Avatar:CooldownUp() and Player:Buff(S.LastStandBuff) or Player:Rage() >= 80 or Player:Rage() >= 55 and S.ShieldSlam:CooldownUp() and Player:Buff(S.ViolentOutburstBuff) and Player:Buff(S.ShieldBlockBuff) or Player:Rage() >= 30 and S.ShieldSlam:CooldownUp() and Player:Buff(S.ViolentOutburstBuff) and Player:Buff(S.LastStandBuff) and Player:Buff(S.ShieldBlockBuff))) then
+	 
+	  if S.IgnorePain:IsReady() and IgnorePainWillNotCap() and (Target:HealthPercentage() >= 20 and (Player:Rage() >= 85 and S.ShieldSlam:CooldownUp() and Player:Buff(S.ShieldBlockBuff) or Player:Rage() >= 60 
+	  and S.DemoralizingShout:CooldownUp() and S.BoomingVoice:IsAvailable() or Player:Rage() >= 70 and S.Avatar:CooldownUp() or Player:Rage() >= 40 and S.DemoralizingShout:CooldownUp() and S.BoomingVoice:IsAvailable() 
+	  and Player:Buff(S.LastStandBuff) or Player:Rage() >= 55 and S.Avatar:CooldownUp() and Player:Buff(S.LastStandBuff) or Player:Rage() >= 80 or Player:Rage() >= 55 and S.ShieldSlam:CooldownUp() and Player:Buff(S.ViolentOutburstBuff) 
+	  and Player:Buff(S.ShieldBlockBuff) or Player:Rage() >= 30 and S.ShieldSlam:CooldownUp() and Player:Buff(S.ViolentOutburstBuff) and Player:Buff(S.LastStandBuff) and Player:Buff(S.ShieldBlockBuff))) then
 		return S.IgnorePain:Cast()
 	end
 
-	  if S.ShieldSlam:IsCastable() and (Player:Buff(S.ViolentOutburstBuff) and Player:Rage() <= 55) then
-		SuggestRageDump(20)
-		return S.ShieldSlam:Cast()
-	end
+	
 	  if (RubimRH.CDsON()) then
 
 		if S.ArcaneTorrent:IsCastable() and (Player:Rage() < 80) and RubimRH.CDsON() then
@@ -552,14 +367,71 @@ end
 
 	  end
 
-	  -- run_action_list,name=aoe,if=spell_targets.thunder_clap>3
-	  if Enemies8y > 3 then
-		local ShouldReturn = Aoe(); if ShouldReturn then return ShouldReturn; end
+	
+
+
+
+
+	-- ignore_pain,if=rage.deficit>=35&buff.ignore_pain.value<health.max*0.3
+	-- if S.IgnorePain:IsReady() and (Player:RageDeficit() >= 35 and IgnorePainValue() < Player:MaxHealth() * 0.3) then
+	-- 	return S.IgnorePain:Cast()
+	-- end
+	-- spear_of_bastion
+	if S.SpearofBastion:IsCastable() and Target:IsInRange(8) and RubimRH.CDsON() then
+	  return S.SpearofBastion:Cast()
+	end
+	-- thunderous_roar
+	if S.ThunderousRoar:IsCastable() and Target:IsInRange(8) and RubimRH.CDsON() then
+	  return S.ThunderousRoar:Cast()
+	end
+	-- ravager
+	if S.Ravager:IsCastable() and (Target:IsInRange(5) or Enemies8y>=2) and RubimRH.CDsON() then
+	  SuggestRageDump(10)
+	  return S.Ravager:Cast()
+	end
+	-- shockwave
+	-- if S.Shockwave:IsCastable() and Target:IsInRange(5) then
+	--   SuggestRageDump(10)
+	--   return S.Shockwave:Cast()
+	-- end
+
+	-- shield_charge
+	if S.ShieldCharge:IsCastable() and Target:IsInRange(5) then
+		return S.ShieldCharge:Cast()
+	end
+
+
+	  if S.IgnorePain:IsReady() and ((Player:RageDeficit() >= 35 and IgnorePainValue() < Player:MaxHealth() * 0.3 * 0.5) or Player:BuffRemains(S.IgnorePain) < Player:GCD()) then
+		return S.IgnorePain:Cast()
+	end
+
+
+
+
+	  if (not ShouldPressShieldBlock()) then
+		-- rend,if=!talent.thunderclap&!talent.blood_and_thunder
+		if S.Rend:IsReady() and ((not S.ThunderClap:IsAvailable()) and not S.BloodandThunder:IsAvailable()) then
+		  return S.Rend:Cast()
+	  end
+		-- revenge,if=rage.deficit>30&active_enemies>=2
+		if S.Revenge:IsReady() and (Player:RageDeficit() > 30 and Enemies8y >= 2) then
+		  return S.Revenge:Cast()
+	  end
 	  end
 
-	  -- call_action_list,name=generic
-	  local ShouldReturn = Generic(); if ShouldReturn then return ShouldReturn; end
+	-- heroic_throw
+	if S.HeroicThrow:IsReady() and not Target:IsInRange(8) and Target:IsInRange(30) and (Target:AffectingCombat() or Player:AffectingCombat()) then
+		return S.HeroicThrow:Cast()
+	end
+	-- titanic_throw
+	if S.TitanicThrow:IsReady() then
+		return S.TitanicThrow:Cast()
+	end
 
+
+
+	  if S.ImpendingVictory:IsReady() then
+		return S.ImpendingVictory:Cast()
 	end
 
 
@@ -567,6 +439,134 @@ end
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	if Cache.EnemiesCount[12]>=3 then
+-- 	thunder_clap,if=dot.rend.remains<=1
+if S.ThunderClap:IsReady() and Target:DebuffRemains(S.ThunderClap)<=1 and Target:IsInRange(8) then
+	return S.ThunderClap:Cast()
+end
+
+-- thunder_clap,if=buff.violent_outburst.up&spell_targets.thunderclap>5&buff.avatar.up&talent.unstoppable_force.enabled
+if S.ThunderClap:IsReady() and Player:Buff(S.ViolentOutburstBuff) and Cache.EnemiesCount[12]>5 and Player:Buff(S.AvatarBuff) and S.UnstoppableForce:IsAvailable() and Target:IsInRange(8) then
+	return S.ThunderClap:Cast()
+end
+
+-- revenge,if=rage>=70&talent.seismic_reverberation.enabled&spell_targets.revenge>=3
+if S.Revenge:IsReady() and Player:Rage()>=70 and S.SeismicReverberation:IsAvailable() and Target:IsInRange(8) then
+	return S.Revenge:Cast()
+end
+
+-- shield_slam,if=rage<=60|buff.violent_outburst.up&spell_targets.thunderclap<=4
+if S.ShieldSlam:IsReady() and Player:Rage()>=70 and S.SeismicReverberation:IsAvailable() and Target:IsInRange(8) then
+	return S.ShieldSlam:Cast()
+end
+-- thunder_clap
+if S.ThunderClap:IsReady() and Target:IsInRange(8) then
+	return S.ThunderClap:Cast()
+end
+
+-- revenge,if=rage>=30|rage>=40&talent.barbaric_training.enabled
+if S.Revenge:IsReady() and Player:Rage()>=30 and S.BarbaricTraining:IsAvailable() and Target:IsInRange(8) then
+	return S.Revenge:Cast()
+end
+end
+
+-- shield_slam
+if S.ShieldSlam:IsReady(8) then
+	return S.ShieldSlam:Cast()
+end
+
+
+-- thunder_clap,if=dot.rend.remains<=1&buff.violent_outburst.down
+if S.ThunderClap:IsCastable() and Target:IsInRange(10) and not Player:Buff(S.ViolentOutburstBuff) then
+	return S.ThunderClap:Cast()
+end
+
+
+-- execute,if=buff.sudden_death.up&talent.sudden_death.enabled
+	if IsUsableSpell('Execute') and S.SuddenDeath:IsAvailable() and  S.Execute:CooldownUp() and Target:IsInRange(8) then
+		return S.Execute:Cast()
+	end
+
+-- execute,if=spell_targets.revenge=1&(talent.massacre.enabled|talent.juggernaut.enabled)&rage>=50
+if IsUsableSpell('Execute') and S.Execute:CooldownUp() and Target:IsInRange(8) and Cache.EnemiesCount[10]==1 and (S.Massacre:IsAvailable() or S.Jugger:IsAvailable()) and Player:Rage()>=50 then
+	return S.Execute:Cast()
+end
+
+
+-- revenge,if=buff.vanguards_determination.down&rage>=40
+if S.Revenge:IsCastable() and Target:IsInRange(8) and Player:Buff(S.VanguardsDetermination) and Player:Rage()>=40 then
+	return S.Revenge:Cast()
+end
+
+
+-- execute,if=spell_targets.revenge=1&rage>=50
+if IsUsableSpell('Execute') and S.Execute:CooldownUp() and Target:IsInRange(8) and Player:Rage()>=50 and Cache.EnemiesCount[10]==1 then
+	return S.Execute:Cast()
+end
+
+
+-- thunder_clap,if=(spell_targets.thunder_clap>1|cooldown.shield_slam.remains&!buff.violent_outburst.up)
+if S.ThunderClap:IsReady() and (Cache.EnemiesCount[10]>1 or not S.ShieldSlam:CooldownUp() and not Player:Buff(S.ViolentOutburstBuff)) and Target:IsInRange(8) then
+	return S.ThunderClap:Cast()
+end
+
+
+-- revenge,if=(rage>=60&target.health.pct>20|buff.revenge.up&target.health.pct<=20&rage<=18&cooldown.shield_slam.remains|buff.revenge.up&target.health.pct>20)
+-- |(rage>=60&target.health.pct>35|buff.revenge.up&target.health.pct<=35&rage<=18&cooldown.shield_slam.remains|buff.revenge.up&target.health.pct>35)&talent.massacre.enabled
+if S.Revenge:IsReady() 
+and ((Player:Rage()>=60 and Target:HealthPercentage()>20 or Player:Buff(S.RevengeBuff) 
+and Target:HealthPercentage()<=20 and Player:Rage()<=18 and not S.ShieldSlam:CooldownUp()
+or Player:Buff(S.RevengeBuff) and Target:HealthPercentage()>20) or 
+(Player:Rage()>=60 and Target:HealthPercentage()>35 or Player:Buff(S.RevengeBuff) and Target:HealthPercentage()<=35 and Player:Rage()<=18 and not S.ShieldSlam:CooldownUp() 
+or Player:Buff(S.RevengeBuff) and Target:HealthPercentage()>35) and S.Massacre:IsAvailable()) then
+	return S.Revenge:Cast()
+end
+
+
+-- execute,if=spell_targets.revenge=1
+if IsUsableSpell('Execute') and S.Execute:CooldownUp() and Target:IsInRange(8) and Cache.EnemiesCount[10]==1 then
+	return S.Execute:Cast()
+end
+
+
+-- revenge
+if S.Revenge:IsReady() and Target:IsInRange(8) and Cache.EnemiesCount[10]==1 then
+	return S.Revenge:Cast()
+end
+
+
+
+-- thunder_clap,if=(spell_targets.thunder_clap>=1|cooldown.shield_slam.remains&buff.violent_outburst.up)
+if S.ThunderClap:IsReady() and Target:IsInRange(8) and (Cache.EnemiesCount[10]>=1 or not S.ShieldSlam:CooldownUp() and Player:Buff(S.ViolentOutburstBuff)) then
+	return S.ThunderClap:Cast()
+end
+
+
+
+-- devastate
+if S.Devastate:IsReady() and Target:IsInRange(8) then
+	return S.Devastate:Cast()
+end
+
+end
   return 0, 135328
 end
 
@@ -577,3 +577,15 @@ local function PASSIVE()
 end
 
 RubimRH.Rotation.SetPASSIVE(73, PASSIVE);
+
+
+
+
+
+
+
+
+
+
+
+
